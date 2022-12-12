3e8803b83490b93788f243240282531ab664a4bc
Blanca Ubis <ubis.blanca@gmail.com>
2022-12-12 20:08:40 +0100

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import '../styles/App.scss';
+
+function App() {
+  // VARIABLES ESTADO
+
+  // USEEFFECT ?
+
+  // FUNCIONES HANDLER
+
+  // FUNCIONES Y VARIABLES QUE AYUDEN A RENDERIZAR HTML
+
+  // HTML EN EL RETURN
+
+  return (
+    <div className="App">
+      <header>
+        <h1 className="title">Rick & Morty</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
