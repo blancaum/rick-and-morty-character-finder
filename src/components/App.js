1ae0ec3a7174e97b9a50432cb55e915fa04da002
Blanca Ubis <ubis.blanca@gmail.com>
2022-12-13 10:43:50 +0100

    Clear local storage to preventgarbage from other apps

--- src/components/App.js
+++ src/components/App.js
@@ -12,17 +12,18 @@ import CharactersList from './CharactersList';
 function App() {
   // VARIABLES ESTADO
   const [data, setData] = useState(ls.get('data', []));
-  const [apiSuccess, setApiSuccess] = useState(ls.get('apiSuccess', false));
 
   // USEEFFECT
   useEffect(() => {
-    if (!ls.isKeyInLocal('data') || !ls.get('apiSuccess', false)) {
+    if (!ls.isKeyInLocal('data') || !ls.get('apiRickMortySuccess', false)) {
       callToApi().then((response) => {
         setData(response.data);
         ls.set('data', response.data);
-        setApiSuccess(response.success);
-        ls.set('apiSuccess', response.success);
+        ls.set('apiRickMortySuccess', response.success);
       });
+    } else if (!ls.isKeyInLocal('apiRickMortySuccess')) {
+      //limpio el Local Storage por si se ha quedado algo guardado de otra app
+      ls.clear();
     }
   }, []);
 
